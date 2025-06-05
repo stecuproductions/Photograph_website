e5a58bbcdf99015d1fc2b1a8fc240f646bb046be
Jakub Stec <stecu03@gmail.com>
2025-06-05 22:37:08 +0200

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Navbar from "../components/navbar";
+import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import References from "../components/References";
 import CTA from "../components/CTA";
